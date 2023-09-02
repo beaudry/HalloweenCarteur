1c60985f65ca3830a59a9bc8bcacd51688739bc8
Charles-André Beaudry <ca_beaudry@yahoo.ca>
2023-09-01 20:22:08 -0400

    Logique pour ajouter les espaces automatiquement

--- src/composants/CarteHalloween.tsx
+++ src/composants/CarteHalloween.tsx
@@ -5,6 +5,18 @@ export type CarteHalloweenProps = {
     carte: Carte
 }
 
+const espaceFineInsecable = "\u202f"
+const ajouterDesEspaces = true;
+
+function ajouterLesEspacesSiDesire(chaineAAjuster: string) {
+    if (!ajouterDesEspaces) {
+        return chaineAAjuster;
+    }
+
+    return chaineAAjuster.replace(/([!?])$/, espaceFineInsecable + "$1");
+
+}
+
 function CarteHalloween({ carte }: CarteHalloweenProps) {
     return <div className="carte">
         <div className="carte-entete">
@@ -12,12 +24,12 @@ function CarteHalloween({ carte }: CarteHalloweenProps) {
             {carte.cycle && <span>{carte.cycle}</span>}
         </div>
         <div className="carte-contenu">
-            <h2>{carte.titre ?? "Aucun titre :("}</h2>
+            <h2>{ajouterLesEspacesSiDesire(carte.titre ?? "Aucun titre :(")}</h2>
             {Array.isArray(carte.description) ?
                 <ul>
                     {carte.description.map(itemDescription => <li key={itemDescription}>{itemDescription}</li>)}
                 </ul>
-                : <p>{carte.description}</p>
+                : <p>{ajouterLesEspacesSiDesire(carte.description)}</p>
             }
         </div>
     </div>
